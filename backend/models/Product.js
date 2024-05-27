af31f703e54533dae609f9a4f63e565d95069d5e
rachataa <rachata.arnankul@gmail.com>
2024-05-27 17:56:50 +0700

    product

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const { gallery } = require("../validators/product");
 
 const schema = new mongoose.Schema(
   {
