39141b733202a7adc3f0cebd2c9d69acff2134cc
rachataa <rachata.arnankul@gmail.com>
2024-05-29 18:20:23 +0700

    model

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -4,41 +4,23 @@ const uniqueValidator = require("mongoose-unique-validator");
 const schema = new mongoose.Schema(
   {
     projectNameTH: { type: String },
-    projectNameEN: { type: String },
-    projectDescriptionTH: { type: String },
-    projectDescriptionEN: { type: String },
     projectDetailTH: { type: String },
-    projectDetailEN: { type: String },
     projectUrl: { type: String, unique: true, required: true },
-    // image: { type: String },
-    // imageAlt: { type: String },
+    image: { type: String },
+    imageAlt: { type: String },
+    gallery: { type: Array },
     sort: { type: Number },
     status: { type: Boolean, default: false },
     projectSeo: {
       titleTH: { type: String },
-      titleEN: { type: String },
       keywordTH: { type: String },
-      keywordEN: { type: String },
       descriptionTH: { type: String },
-      descriptionEN: { type: String },
     },
   },
   { timestamps: true }
 );
 
 schema.plugin(uniqueValidator);
-
-schema.pre("validate", function (next) {
-  if (!this.serviceNameTH && !this.serviceNameEN)
-    return next(new Error("Please fill serviceName in at least 1 field."));
-
-  if (!this.serviceDescriptionTH && !this.serviceDescriptionEN)
-    return next(
-      new Error("Please fill serviceDescription in at least 1 field.")
-    );
-
-  next();
-});
 
 schema.pre("save", async function (next) {
   if (!this.sort) {
@@ -57,23 +39,17 @@ schema.methods.toJSON = function () {
   return {
     id: this._id,
     projectNameTH: this.projectNameTH,
-    projectNameEN: this.projectNameEN,
-    projectDescriptionTH: this.projectDescriptionTH,
-    projectDescriptionEN: this.projectDescriptionEN,
     projectDetailTH: this.projectDetailTH,
-    projectDetailEN: this.projectDetailEN,
     projectUrl: this.projectUrl,
-    // image: this.image,
-    // imageAlt: this.imageAlt,
+    image: this.image,
+    imageAlt: this.imageAlt,
+    gallery: this.gallery,
     sort: this.sort,
     status: this.status,
     projectSeo: {
       titleTH: this.projectSeo["titleTH"],
-      titleEN: this.projectSeo["titleEN"],
       keywordTH: this.projectSeo["keywordTH"],
-      keywordEN: this.projectSeo["keywordEN"],
       descriptionTH: this.projectSeo["descriptionTH"],
-      descriptionEN: this.projectSeo["descriptionEN"],
     },
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
