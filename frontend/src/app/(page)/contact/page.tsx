328fc56b65fc744886a0fb6cdfa71ca586bb5862
HOCKIIE <spw.kgs@gmail.com>
2024-05-30 13:48:23 +0700

    contact page

--- frontend/src/app/(page)/contact/page.tsx
+++ frontend/src/app/(page)/contact/page.tsx
@@ -2,6 +2,7 @@ import Cover from "@/components/main/Cover/Cover";
 import ForeignBranch from "@/components/main/ForeignBranch/ForeignBranch";
 import Loading from "@/components/main/Loading/Loading";
 import Contact from "@/components/main/Contact/page";
+import Image from "next/image";
 
 export default function ContactPage() {
 
@@ -15,6 +16,17 @@ export default function ContactPage() {
       <div className="container mx-auto">
         <Contact />
         <ForeignBranch title="สาขาต่างประเทศ" color="#0EA3DC" />
+        <div className="mt-5 flex justify-center p-6 border-t-2 border-slate-200">
+          <Image
+              className="aspect-[4/2] object-cover"
+              alt={`บริษัท เร้นท์ (ประเทศไทย) จำกัด`}
+              src={`/Rectangle 136.png`}
+              width={834}
+              height={436}
+              quality={100}
+              loading="lazy"
+          />
+        </div>
       </div>
     </>
   );
