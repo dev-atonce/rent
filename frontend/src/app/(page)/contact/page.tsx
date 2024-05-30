1ce31731cb01fd5f819f151a8f11c2370abcbf74
HOCKIIE <spw.kgs@gmail.com>
2024-05-30 13:37:38 +0700

    contact page

--- frontend/src/app/(page)/contact/page.tsx
+++ frontend/src/app/(page)/contact/page.tsx
@@ -1,18 +1,10 @@
 import Cover from "@/components/main/Cover/Cover";
 import ForeignBranch from "@/components/main/ForeignBranch/ForeignBranch";
 import Loading from "@/components/main/Loading/Loading";
+import Contact from "@/components/main/Contact/page";
 
 export default function ContactPage() {
-  const branch = [
-    {"branch":"สาขาชลบุรี","tel":"โทร. 033-048-248"},
-    {"branch":"สาขาบ่อวิน","tel":"โทร. 038-959-343"},
-    {"branch":"สาขามาบตาพุด","tel":"โทร. 033-017-791"},
-    {"branch":"สาขาสมุทรปราการ","tel":"โทร. 02-136-7104"},
-    {"branch":"สาขารังสิต","tel":"โทร. 02-090-2623"},
-    {"branch":"สาขาสมุทรสาคร","tel":"โทร. 034-861-020"},
-    {"branch":"สำนักงานใหญ่","tel":"โทร. 02-0177200"},
-    {"branch":"บริษัท เร้นท์ เทรด แอนด์ เซอร์วิส","tel":"โทร. 02-017-7217"},
-  ];
+
   return (
     <>
       <Loading />
@@ -21,6 +13,7 @@ export default function ContactPage() {
         prevPage={{ pageName: "หน้าแรก", url: "/" }}
       />
       <div className="container mx-auto">
+        <Contact />
         <ForeignBranch title="สาขาต่างประเทศ" color="#0EA3DC" />
       </div>
     </>
---
+++ frontend/src/components/main/Contact/page.tsx
@@ -0,0 +1,22 @@
+import { Row } from "antd";
+import ContactCard from "./Card";
+
+export default function Contact() {
+    const list = [
+        {"thumbnail":"/branch/branch_0.png","branch":"สาขาชลบุรี","telephone":"โทร. 033-048-248"},
+        {"thumbnail":"/branch/branch_1.png","branch":"สาขาบ่อวิน","telephone":"โทร. 038-959-343"},
+        {"thumbnail":"/branch/branch_2.png","branch":"สาขามาบตาพุด","telephone":"โทร. 033-017-791"},
+        {"thumbnail":"/branch/branch_3.png","branch":"สาขาสมุทรปราการ","telephone":"โทร. 02-136-7104"},
+        {"thumbnail":"/branch/branch_4.png","branch":"สาขารังสิต","telephone":"โทร. 02-090-2623"},
+        {"thumbnail":"/branch/branch_5.png","branch":"สาขาสมุทรสาคร","telephone":"โทร. 034-861-020"},
+        {"thumbnail":"/branch/branch_6.png","branch":"สำนักงานใหญ่","telephone":"โทร. 02-0177200"},
+        {"thumbnail":"/branch/branch_7.png","branch":"บริษัท เร้นท์ เทรด แอนด์ เซอร์วิส","telephone":"โทร. 02-017-7217"},
+      ];
+  return (
+    <div>
+        <Row gutter={[16, 16]}>
+            <ContactCard data={list}></ContactCard>
+        </Row>
+    </div>
+  )
+}
