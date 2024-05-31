69dbb8de43b3a82b0aab4e2954762497a9c257dd
rachataa <rachata.arnankul@gmail.com>
2024-05-31 16:21:41 +0700

    project

--- backend/controllers/webpanel/projectPanel.controller.js
+++ backend/controllers/webpanel/projectPanel.controller.js
@@ -98,12 +98,15 @@ const methods = {
 
   async onDeleteGallery(req, res) {
     try {
-        let result = await Project.deleteGallery(req.params.id, req.params.position);
-        res.success(result);
+      let result = await Project.deleteGallery(
+        req.params.id,
+        req.params.position
+      );
+      res.success(result);
     } catch (error) {
-        res.error(error);
+      res.error(error);
     }
-},
+  },
 };
 
 module.exports = { ...methods };
