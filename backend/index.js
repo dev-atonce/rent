999454320cce6b6c7c82841585c88a0dd15d0a52
rachataa <rachata.arnankul@gmail.com>
2024-06-12 16:55:48 +0700

    16.55

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 require("./configs/databases");
 
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Passport
 require("./configs/passport");
