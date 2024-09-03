d745e224b8911bc1e1af61a7568bfb0fb55e6aae
rachataa <rachata.arnankul@gmail.com>
2024-09-03 15:36:33 +0700

    fix api

--- backend/controllers/page/aboutUsPage.controller.js
+++ backend/controllers/page/aboutUsPage.controller.js
@@ -3,7 +3,7 @@ const aboutUs = require("../../services/page/aboutUsPage.service");
 const methods = {
   async onGet(req, res) {
     try {
-      let result = await aboutUs.find(req);
+      let result = await aboutUs.find(req, res);
       res.success(result);
     } catch (error) {
       res.error(error);
